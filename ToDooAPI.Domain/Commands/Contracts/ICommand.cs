4b53dfccccaf46d69a405f329a903a6e6abeaf37
BrunoFaria2021 <bmc_ti@outlook.com>
2023-02-23 02:59:11 -0300

    Criando Entities e Commands

---
+++ ToDooAPI.Domain/Commands/Contracts/ICommand.cs
@@ -0,0 +1,10 @@
+﻿using Flunt.Validations;
+
+namespace ToDooAPI.Domain.Commands.Contracts
+{
+    public interface ICommand : IValidatable
+    {
+        
+
+    }
+}
--- ToDooAPI.Domain/Entities/Base.cs
+++ ToDooAPI.Domain/Entities/Base.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace ToDooAPI.Domain.Entities
 {
-    public abstract class Base
+    public abstract class Base : IEquatable<Base>
     {
+        public Base() 
+        {
+            Id = Guid.NewGuid();
+        }
         public Guid Id { get; private set; }
+
+        public bool Equals(Base other)
+        {
+            return Id == other.Id;
+        }
     }
 }
