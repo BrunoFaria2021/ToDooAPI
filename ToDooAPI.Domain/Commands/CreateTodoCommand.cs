9ebe5e3c9c7eb9706c63d6cc7d2130d8b60b3496
BrunoFaria2021 <bmc_ti@outlook.com>
2023-02-23 03:31:14 -0300

    Refatorando Commands

---
+++ ToDooAPI.Domain/Commands/CreateTodoCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ToDooAPI.Domain.Commands.Contracts;
+
+namespace ToDooAPI.Domain.Commands
+{
+    public class CreateTodoCommand : ICommand
+    {
+        public CreateTodoCommand() 
+        {
+        }
+
+        public CreateTodoCommand(string title, string user, DateTime date)
+        {
+            Title = title;
+            User = user;
+            Date = date;
+        }
+        public string Title { get; set; }
+        public string User { get; set; }
+        public DateTime Date { get; set; }
+
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
